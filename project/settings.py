97e126f80fff3b3658a6e300c2c257c8c7980a9d
Matt Kimball <matthewkimball@outlook.com>
2023-07-27 22:44:42 -0400

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -12,13 +12,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import environ
 import os
+from pathlib import Path
 
 env = environ.Env(
     DEBUG=(bool, False)
 )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/

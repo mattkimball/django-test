1213ec70e6496f2ef770ef27935e7906540640e9
Matt Kimball <matthewkimball@outlook.com>
2023-07-30 11:42:22 -0400

    Update urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('task-list', views.TaskListView.as_view(), name='task-list'),
     path('add-task', views.task_create_view, name='task-create'),
-    path('<int:id>/toggle', views.task_toggle, name='task-toggle'),
+    path('tasks/<int:id>/toggle', views.task_toggle, name='task-toggle'),
 ]
